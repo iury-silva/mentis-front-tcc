2e1b96c66fe52629ee7ea8dfe0965d3aa589214e
iury-silva <iury2002silva@hotmail.com>
2025-09-22 11:36:37 -0300

    feat: adicionar suporte ao Vercel Analytics e Speed Insights no componente App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 // src/App.tsx
 import AppRoutes from '@/routes'; // Using @/ alias
+import {
+  Analytics,
+} from '@vercel/analytics/react';
+
+import {
+  SpeedInsights
+} from '@vercel/speed-insights/react';
 
 function App() {
-  return <AppRoutes />;
+  return (
+    <>
+      <AppRoutes />
+      <Analytics />
+      <SpeedInsights />
+    </>
+  );
 }
 
 export default App;
