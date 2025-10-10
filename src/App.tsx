10faaf19a666e9dce01aa06ffdc73ce4f96d1629
iury-silva <iury2002silva@hotmail.com>
2025-10-10 14:32:46 -0300

    fix: comentar importações de Analytics e SpeedInsights no App

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 // src/App.tsx
 import AppRoutes from '@/routes'; // Using @/ alias
-import {
-  Analytics,
-} from '@vercel/analytics/react';
+// import {
+//   Analytics,
+// } from '@vercel/analytics/react';
 
-import {
-  SpeedInsights
-} from '@vercel/speed-insights/react';
+// import {
+//   SpeedInsights
+// } from '@vercel/speed-insights/react';
 
 function App() {
   return (
     <>
       <AppRoutes />
-      <Analytics />
-      <SpeedInsights />
+      {/* <Analytics /> */}
+      {/* <SpeedInsights /> */}
     </>
   );
 }
